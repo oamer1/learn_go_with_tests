aabc077480d95e25e09c23106324547e1167e624
Omar Amer <oamer@windowslive.com>
2022-03-07 00:56:34 +0200

    Refactor test, use helper

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -4,21 +4,29 @@ import "testing"
 
 func TestHello(t *testing.T) {
 
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+
+		// By doing this when it fails the line number reported will be in our function call rather than
+
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+
+	}
+
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
+
 	})
 }
