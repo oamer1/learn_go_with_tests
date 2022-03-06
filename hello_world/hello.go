20b612af47cd6581b77b7c1b699df104663a876b
Omar Amer <oamer@windowslive.com>
2022-03-07 01:07:22 +0200

    Support french

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -1,10 +1,21 @@
 package main
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const french = "French"
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if language == spanish {
+		return spanishHelloPrefix + name
+	}
+	if language == french {
+		return frenchHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
