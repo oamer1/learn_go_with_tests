a8ee13f216da09f7152b8b523d7548a737811880
Omar Amer <oamer@windowslive.com>
2022-03-07 09:04:42 +0200

    Passing code

--- structs_metods_interfaces/shapes.go
+++ structs_metods_interfaces/shapes.go
@@ -2,5 +2,9 @@ package main
 
 // Perimeter returns the perimeter of a rectangle.
 func Perimeter(width, height float64) float64 {
-	return 0
+	return 2 * (width + height)
+}
+
+func Area(width float64, height float64) float64 {
+	return width * height
 }
