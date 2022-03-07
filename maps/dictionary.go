9138c66424f3dbd83f0c61b918a2aabcf899a3fb
Omar Amer <oamer@windowslive.com>
2022-03-08 01:44:20 +0200

    Green, minimal code

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,8 +1,14 @@
 package main
 
+import "errors"
+
 // Dictionary type which acts as a thin wrapper around map. With the custom type defined
 type Dictionary map[string]string
 
 func (d Dictionary) Search(word string) (string, error) {
-	return d[word], nil
+	defintion, ok := d[word]
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+	return defintion, nil
 }
