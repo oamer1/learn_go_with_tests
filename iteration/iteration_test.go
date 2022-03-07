6a96d4c590b55e879e74a52d9cc9b4bc1b538b61
Omar Amer <oamer@windowslive.com>
2022-03-07 04:53:34 +0200

    Add benchmark

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -10,3 +10,12 @@ func TestRepeat(t *testing.T) {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
+
+// Writing benchmarks in Go is another first-class feature of the language and it is very similar to writing tests.
+
+func BenchmarkRepeat(b *testing.B) {
+	// t runs b.N times and measures how long it takes.
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
