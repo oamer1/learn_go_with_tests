8fd644b678b88bcb3d7394641ae3e9875807395c
Omar Amer <oamer@windowslive.com>
2022-03-07 09:20:12 +0200

    Write methods

--- structs_metods_interfaces/shapes.go
+++ structs_metods_interfaces/shapes.go
@@ -1,15 +1,34 @@
 package main
 
+import "math"
+
+// Unlike Java and C++, Go does not allow function overloading
+// func Area(circle Circle) float64       {}
+// func Area(rectangle Rectangle) float64 {}
+
+// so
+// You can have functions with the same name declared in different packages. So we could create our Area(Circle) in a new package, but that feels overkill here.
+// We can define methods on our newly defined types instead.
+
+// A method is a function with a receiver
+
+type Circle struct {
+	Radius float64
+}
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
 // Perimeter returns the perimeter of a rectangle.
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
