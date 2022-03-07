0190d5cfef3ca1529d4273a46e2d97584af60ca3
Omar Amer <oamer@windowslive.com>
2022-03-07 14:54:54 +0200

    Refactor Bitcoin balance

--- pointers_errors/wallet.go
+++ pointers_errors/wallet.go
@@ -2,16 +2,19 @@ package main
 
 // In Go if a symbol (variables, types, functions et al) starts with a lowercase symbol then it is private outside the package it's defined in.
 // In Go, when you call a function or a method the arguments are copied.
+
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
 // In Go no need for dereferencing
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	// Same as (*w).balance
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
--- pointers_errors/wallet_test.go
+++ pointers_errors/wallet_test.go
@@ -9,13 +9,13 @@ func TestWallet(t *testing.T) {
 
 	wallet := Wallet{}
 
-	wallet.Deposit(3)
+	wallet.Deposit(Bitcoin(3))
 
 	got := wallet.Balance()
 	// address of balance in test is 0xc000018190
 	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
 
-	want := 3
+	want := Bitcoin(3)
 
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
