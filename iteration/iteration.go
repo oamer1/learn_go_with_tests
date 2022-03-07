e4ba597586b694731a84e5cf423b42a0dcbfd655
Omar Amer <oamer@windowslive.com>
2022-03-07 04:47:34 +0200

    Write minimal code

---
+++ iteration/iteration.go
@@ -0,0 +1,6 @@
+// To do stuff repeatedly in Go, you'll need for. In Go there are no while, do, until keywords, you can only use for. Which is a good thing!
+package iteration
+
+func Repeat(char string) string {
+	return ""
+}
