87005cb368e0d9a6387187564600852f3bce9ca8
Omar Amer <oamer@windowslive.com>
2022-03-08 01:41:35 +0200

    Minimal code

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -3,6 +3,6 @@ package main
 // Dictionary type which acts as a thin wrapper around map. With the custom type defined
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	return d[word], nil
 }
