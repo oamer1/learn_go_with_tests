92086a638a67302b6c732846f0e1c4c440a0a4d5
Omar Amer <oamer@windowslive.com>
2022-03-07 05:00:17 +0200

    Add example

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,6 +1,9 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
 	repeated := Repeat("a")
@@ -18,4 +21,10 @@ func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Repeat("a")
 	}
+}
+
+func ExampleRepeat() {
+	rep := Repeat("n")
+	fmt.Println(rep)
+	// Output:nnnnn
 }
