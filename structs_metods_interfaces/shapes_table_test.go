884958f6bb9770362353a95570e1af9f3ea7b182
Omar Amer <oamer@windowslive.com>
2022-03-07 11:19:47 +0200

    Refactor better names

--- structs_metods_interfaces/shapes_table_test.go
+++ structs_metods_interfaces/shapes_table_test.go
@@ -19,19 +19,21 @@ func TestArea1(t *testing.T) {
 
 	// "anonymous struct"
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		shape   Shape
+		hasArea float64
 	}{
-		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
-		{shape: Circle{Radius: 10}, want: 314.1592653589793},
-		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+		{shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
 		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
+		if got != tt.hasArea {
+			t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
 		}
 	}
 
 }
+
+// The %#v format string will print out our struct with the values in its field, so the developer can see at a glance the properties that are being tested.
