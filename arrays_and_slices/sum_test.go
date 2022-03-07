e6007ad8b4063a3dfe02ddb09eb0a24fe2bf153a
Omar Amer <oamer@windowslive.com>
2022-03-07 05:32:25 +0200

    Add slice test

--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -6,14 +6,31 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
-	// Alternatively [...]int{1, 2, 3, 4, 5}
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
 
-	got := Sum(numbers)
-	want := 15
+		// Alternatively [...]int{1, 2, 3, 4, 5}
+		numbers := [5]int{1, 2, 3, 4, 5}
 
-	// %v placeholder to print the "default" format, which works well for arrays.
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+		got := Sum(numbers)
+		want := 15
+
+		// %v placeholder to print the "default" format, which works well for arrays.
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+	t.Run("collections of any size", func(t *testing.T) {
+		// Use slice
+
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
 }
