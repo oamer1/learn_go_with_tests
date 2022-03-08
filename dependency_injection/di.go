a0d688ff0e20f7a09c78170934c3ad5d8312d1a1
Omar Amer <oamer@windowslive.com>
2022-03-08 06:47:37 +0200

    Red minimal code

---
+++ dependency_injection/di.go
@@ -0,0 +1,19 @@
+/* func Greet(name string) {
+	fmt.Printf("Hello, %s", name)
+}
+
+
+What we need to do is to be able to inject (which is just a fancy word for pass in) the dependency of printing.
+Our function doesn't need to care _where** or **how** the printing happens, so we should accept an **interface_** rather than a concrete type.
+*/
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func Greet(writer *bytes.Buffer, name string) {
+	fmt.Printf("Hello, %s", name)
+}
