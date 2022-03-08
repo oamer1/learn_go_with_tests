a458fe3e1fd84849a0e1e5128758af097d6fda41
Omar Amer <oamer@windowslive.com>
2022-03-08 06:29:23 +0200

    Refactor errors to implement error interface

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,22 +1,31 @@
 package main
 
-import "errors"
-
 // So when you pass a map to a function/method, you are indeed copying it, but just the pointer part, not the underlying data structure that contains the data.
 // Dictionary type which acts as a thin wrapper around map. With the custom type defined
 type Dictionary map[string]string
 
-var (
-	ErrNotFound   = errors.New("could not find the word you were looking for")
-	ErrWordExists = errors.New("cannot add word because it already exists")
+// We made the errors constant; this required us to create our own DictionaryErr type which implements the error interface.
+
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
 )
 
+type DictionaryErr string
+
+// Implement error interface
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+// Search find a word in the dictionary.
 func (d Dictionary) Search(word string) (string, error) {
-	defintion, ok := d[word]
+	definition, ok := d[word]
 	if !ok {
 		return "", ErrNotFound
 	}
-	return defintion, nil
+
+	return definition, nil
 }
 
 func (d Dictionary) Add(word, definition string) error {
