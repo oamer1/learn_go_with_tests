75f1d8a38f5e0a921fcaa524bd8fc451f847869c
Omar Amer <oamer@windowslive.com>
2022-03-08 06:50:28 +0200

    Red failing

--- dependency_injection/di.go
+++ dependency_injection/di.go
@@ -12,8 +12,13 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"os"
 )
 
 func Greet(writer *bytes.Buffer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func main() {
+	Greet(os.Stdout, "Elodie")
 }
