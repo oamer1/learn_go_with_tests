1b969302fa2cdc619e83696e18f907621f686264
Omar Amer <oamer@windowslive.com>
2022-03-09 06:37:42 +0200

    Replace with mocks

--- Select/racer_test.go
+++ Select/racer_test.go
@@ -1,16 +1,37 @@
 package racer
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+// In the mocking and dependency injection chapters, we covered how ideally we don't want to be relying on external services to test our code because they can be
+// Slow
+// Flaky
+// Can't test edge cases
 
 func TestRacer(t *testing.T) {
 
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.dev"
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 
 	want := fastURL
 	got := Racer(slowURL, fastURL)
-
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
+
+	slowServer.Close()
+	fastServer.Close()
+
 }
