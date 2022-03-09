7df45d0d4219f6d2e28df43f6b466f1bd084ba30
Omar Amer <oamer@windowslive.com>
2022-03-09 06:01:08 +0200

    Green, safe from race condition

--- concurrency/check_websites.go
+++ concurrency/check_websites.go
@@ -1,15 +1,21 @@
 package concurrency
 
-import "time"
-
 // WebsiteChecker checks a url, returning a bool.
 type WebsiteChecker func(string) bool
+
+// As we don't need either value to be named,
+type result struct {
+	string
+	bool
+}
 
 // CheckWebsites takes a WebsiteChecker and a slice of urls and returns  a map.
 // of urls to the result of checking each url with the WebsiteChecker function
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
+	// Parralized part
 	// Anonymous functions have
 	// 1-executed at the same time that they're declared
 	// 2- lexical scope they are defined
@@ -18,13 +24,18 @@ func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 		// Generate WARNING: DATA RACE if run "go test -race"
 		url := url
 		go func() {
-			results[url] = wc(url)
+			resultChannel <- result{url, wc(url)}
 		}()
 
 	}
 
-	// Allow coroutines to finish
-	time.Sleep(2 * time.Second)
+	// Linear part to avoid race condition
+	// ensuring that it happens one at a time
+	// go test -race now passes
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.string] = r.bool
+	}
 
 	return results
 }
