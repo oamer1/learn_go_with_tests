acde364a26558aea96cf2e59db30562d64138393
Omar Amer <oamer@windowslive.com>
2022-03-09 09:07:49 +0200

    Use select statements

--- Select/racer.go
+++ Select/racer.go
@@ -2,22 +2,26 @@ package racer
 
 import (
 	"net/http"
-	"time"
 )
 
 func Racer(a, b string) (winner string) {
 
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
-
-	if aDuration < bDuration {
+	select {
+	case <-ping(a):
 		return a
+	case <-ping(b):
+		return b
 	}
-	return b
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+// Always make channels For channels the zero value is nil and if you try and send to it with <- it will block forever because you cannot send to nil channels
+// Why struct{} and not another type like a bool? Well, a chan struct{} is the smallest data type available from a memory
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
