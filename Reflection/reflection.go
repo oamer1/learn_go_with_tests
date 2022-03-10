c60029f80aa1ffc53c0c6573c4532c5cd661698d
Omar Amer <oamer@windowslive.com>
2022-03-10 08:27:24 +0200

    Green fix pointer issue

--- Reflection/reflection.go
+++ Reflection/reflection.go
@@ -6,6 +6,11 @@ import "reflect"
 func walk(x interface{}, fn func(input string)) {
 	val := reflect.ValueOf(x)
 
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	// NumField on a pointer Value
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
 
