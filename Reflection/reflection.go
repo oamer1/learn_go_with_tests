726d718afb5cc2137d138a8e0263ac18e7c5cc41
Omar Amer <oamer@windowslive.com>
2022-03-10 09:14:22 +0200

    Refactor

--- Reflection/reflection.go
+++ Reflection/reflection.go
@@ -2,31 +2,23 @@ package main
 
 import "reflect"
 
-// This code is very unsafe and very naive
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
-	if val.Kind() == reflect.Slice {
+	switch val.Kind() {
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walk(val.Field(i).Interface(), fn)
+		}
+	case reflect.Slice:
 		for i := 0; i < val.Len(); i++ {
 			walk(val.Index(i).Interface(), fn)
 		}
-		return
-	}
-
-	// NumField on a pointer Value
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			walk(field.Interface(), fn)
-
-		}
-
+	case reflect.String:
+		fn(val.String())
 	}
 }
+
 func getValue(x interface{}) reflect.Value {
 	val := reflect.ValueOf(x)
 
