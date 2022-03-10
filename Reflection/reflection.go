1f84fb3a96b52a9b29001251d44f000e6f77300f
Omar Amer <oamer@windowslive.com>
2022-03-10 08:00:35 +0200

    Green

--- Reflection/reflection.go
+++ Reflection/reflection.go
@@ -5,6 +5,9 @@ import "reflect"
 // This code is very unsafe and very naive
 func walk(x interface{}, fn func(input string)) {
 	val := reflect.ValueOf(x)
-	field := val.Field(0)
-	fn(field.String())
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fn(field.String())
+	}
 }
