8a53d68f7884e9df7360a4354055873738c7c3e7
Omar Amer <oamer@windowslive.com>
2022-03-10 07:39:52 +0200

    Green

--- Reflection/reflection.go
+++ Reflection/reflection.go
@@ -1,5 +1,10 @@
 package main
 
+import "reflect"
+
+// This code is very unsafe and very naive
 func walk(x interface{}, fn func(input string)) {
-	fn("I still can't believe South Korea beat Germany 2-0 to put them last in their group")
+	val := reflect.ValueOf(x)
+	field := val.Field(0)
+	fn(field.String())
 }
