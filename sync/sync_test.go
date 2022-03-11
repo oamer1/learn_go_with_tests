e59594200ac9f7690f216bc63dd01a704ce6cace
Omar Amer <oamer@windowslive.com>
2022-03-11 08:05:24 +0200

    Red add concurrency test

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,6 +1,9 @@
 package sync
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
@@ -9,6 +12,29 @@ func TestCounter(t *testing.T) {
 		counter.Inc()
 		counter.Inc()
 		assertCounter(t, counter, 3)
+
+	})
+	t.Run("Runs safely concurrently", func(t *testing.T) {
+
+		wantedCount := 1000
+		counter := Counter{}
+
+		// A WaitGroup waits for a collection of goroutines to finish.
+		// The main goroutine calls Add to set the number of goroutines to wait for.
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+
+			}()
+		}
+
+		// block until all routines finish
+		wg.Wait()
+		assertCounter(t, counter, wantedCount)
 
 	})
 }
