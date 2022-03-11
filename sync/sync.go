b93dd3b04d140f367ddfe31436c847c474a9f332
Omar Amer <oamer@windowslive.com>
2022-03-11 08:07:56 +0200

    Green counter uses mutex

--- sync/sync.go
+++ sync/sync.go
@@ -1,10 +1,16 @@
 package sync
 
+import "sync"
+
 type Counter struct {
 	value int
+	mu    sync.Mutex
 }
 
 func (c *Counter) Inc() {
+	//  All the other goroutines will have to wait for it to be Unlocked before getting access.
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.value++
 }
 
