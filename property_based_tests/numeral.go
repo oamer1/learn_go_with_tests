82d4b8bfa8469eb10d089744827a301b43368148
Omar Amer <oamer@windowslive.com>
2022-03-12 14:34:18 +0200

    Green add 5 case

--- property_based_tests/numeral.go
+++ property_based_tests/numeral.go
@@ -4,13 +4,17 @@ import "strings"
 
 func ConvertToRoman(arabic int) string {
 
-	if arabic == 4 {
-		return "IV"
-	}
-
 	var result strings.Builder
 
-	for i := 0; i < arabic; i++ {
+	for i := arabic; i > 0; i-- {
+		if i == 5 {
+			result.WriteString("V")
+			break
+		}
+		if i == 4 {
+			result.WriteString("IV")
+			break
+		}
 		result.WriteString("I")
 	}
 
