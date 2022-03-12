a2e78b81915bec321c740602e4638171372d8f14
Omar Amer <oamer@windowslive.com>
2022-03-12 14:28:05 +0200

    Green add III case

--- property_based_tests/numeral.go
+++ property_based_tests/numeral.go
@@ -1,7 +1,9 @@
 package propertybasedtests
 
 func ConvertToRoman(arabic int) string {
-	if arabic == 2 {
+	if arabic == 3 {
+		return "III"
+	} else if arabic == 2 {
 		return "II"
 	}
 	return "I"
