d13f18d989a7b6ab92c6db00d82494ece647157c
Omar Amer <oamer@windowslive.com>
2022-03-13 11:33:37 +0200

    Green

--- maths/clockface_test.go
+++ maths/clockface_test.go
@@ -1,4 +1,4 @@
-package clockface_test
+package clockface
 
 import (
 	"testing"
@@ -9,8 +9,8 @@ func TestSecondHandAtMidnight(t *testing.T) {
 
 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
-	want := clockface.Point{x: 150, Y: 150 - 90}
-	got := clockface.SecondHand(tm)
+	want := Point{X: 150, Y: 150 - 90}
+	got := SecondHand(tm)
 
 	if got != want {
 		t.Errorf("Got %v, wanted %v", got, want)
