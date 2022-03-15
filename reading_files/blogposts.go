ded9d6afa5d1d13da5bfbcfe9592d1b78640733d
Omar Amer <oamer@windowslive.com>
2022-03-15 11:12:38 +0200

    Green fs.Fs interface

--- reading_files/blogposts.go
+++ reading_files/blogposts.go
@@ -1,10 +1,17 @@
 package blogposts
 
-import "testing/fstest"
+import (
+	"io/fs"
+)
 
 type Post struct {
 }
 
-func NewPostsFromFS(fileSystem fstest.MapFS) []Post {
-	return []Post{{}, {}}
+func NewPostsFromFS(fileSystem fs.FS) []Post {
+	dir, _ := fs.ReadDir(fileSystem, ".")
+	var posts []Post
+	for range dir {
+		posts = append(posts, Post{})
+	}
+	return posts
 }
