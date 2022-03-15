5ee99aca8b60dc79d81d7a69898df2d670b2738c
Omar Amer <oamer@windowslive.com>
2022-03-15 13:19:43 +0200

    Red

--- reading_files/blogposts.go
+++ reading_files/blogposts.go
@@ -1,13 +1,15 @@
 package blogposts
 
 import (
+	"io"
 	"io/fs"
 )
 
 type Post struct {
+	Title string
 }
 
-func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
+func NewPostsFromsFS(fileSystem fs.FS) ([]Post, error) {
 	dir, err := fs.ReadDir(fileSystem, ".")
 	if err != nil {
 		return nil, err
@@ -18,4 +20,27 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 		posts = append(posts, Post{})
 	}
 	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
+
+	postFile, err := fileSystem.Open(f.Name())
+
+	if err != nil {
+		return Post{}, err
+
+	}
+	defer postFile.Close()
+	return newPost(postFile)
+
+}
+
+func newPost(postFile fs.File) (Post, error) {
+	postData, err := io.ReadAll(postFile)
+	if err != nil {
+		return Post{}, err
+	}
+
+	post := Post{Title: string(postData)[7:]}
+	return post, nil
 }
--- reading_files/blogposts_test.go
+++ reading_files/blogposts_test.go
@@ -21,11 +21,14 @@ func (s StubFailingFS) open(name string) (fs.File, error) {
 func TestNewBlogPosts(t *testing.T) {
 	// in-memory file system for testing
 	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("hi")},
-		"hello-world2.md": {Data: []byte("hola")},
+		"hello world.md":  {Data: []byte("Title: Post 1")},
+		"hello-world2.md": {Data: []byte("Title: Post 2")},
 	}
 
-	posts, err := blogposts.NewPostsFromFS(fs)
+	posts, err := blogposts.NewPostsFromsFS(fs)
+
+	got := posts[0]
+	want := blogposts.Post{Title: "Post 1"}
 
 	if err != nil {
 		t.Fatal(err)
