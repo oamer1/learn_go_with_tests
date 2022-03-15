b05dc98995218dee2c1c50c079eade7a21957087
Omar Amer <oamer@windowslive.com>
2022-03-15 11:21:36 +0200

    Add failing file stub

--- reading_files/blogposts_test.go
+++ reading_files/blogposts_test.go
@@ -3,10 +3,20 @@ package blogposts_test
 // By appending _test to our intended package name, we only access exported members from our package - just like a real user of our package.
 
 import (
+	"errors"
+	"io/fs"
 	blogposts "learn_go_with_tests/reading_files"
 	"testing"
 	"testing/fstest"
 )
+
+// inject a failing fs.FS test-double to make fs.ReadDir return an error.
+type StubFailingFS struct {
+}
+
+func (s StubFailingFS) open(name string) (fs.File, error) {
+	return nil, errors.New("Always failing file")
+}
 
 func TestNewBlogPosts(t *testing.T) {
 	// in-memory file system for testing
