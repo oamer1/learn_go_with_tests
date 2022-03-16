96d2402ed0c2e213ccb1d2acf9d749798d4d2a57
Omar Amer <oamer@windowslive.com>
2022-03-16 17:39:45 +0200

    Green approval testing

--- blogrenderer/renderer.go
+++ blogrenderer/renderer.go
@@ -1,23 +1,18 @@
 package blogrenderer
 
 import (
+	"embed"
 	"html/template"
 	"io"
 )
 
-const (
-	postTemplate = `<h1>{{.Title}}</h1><p>{{.Description}}</p>Tags: <ul>{{range .Tags}}<li>{{.}}</li>{{end}}</ul>`
+var (
+	//go:embed "templates/*"
+	postTemplates embed.FS
 )
 
-// if you're continuing from the read files chapter, you shouldn't redefine this
-type Post struct {
-	Title, Description, Body string
-	Tags                     []string
-}
-
 func Render(w io.Writer, p Post) error {
-	templ, err := template.New("blog").Parse(postTemplate)
-
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
 	if err != nil {
 		return err
 	}
@@ -25,5 +20,6 @@ func Render(w io.Writer, p Post) error {
 	if err := templ.Execute(w, p); err != nil {
 		return err
 	}
+
 	return nil
 }
--- blogrenderer/renderer_test.go
+++ blogrenderer/renderer_test.go
@@ -4,10 +4,11 @@ import (
 	"bytes"
 	"learn_go_with_tests/blogrenderer"
 	"testing"
+
+	approvals "github.com/approvals/go-approval-tests"
 )
 
 func TestRender(t *testing.T) {
-
 	var (
 		aPost = blogrenderer.Post{
 			Title:       "hello world",
@@ -16,20 +17,14 @@ func TestRender(t *testing.T) {
 			Tags:        []string{"go", "tdd"},
 		}
 	)
+
 	t.Run("it converts a single post into HTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
-		err := blogrenderer.Render(&buf, aPost)
 
-		if err != nil {
+		if err := blogrenderer.Render(&buf, aPost); err != nil {
 			t.Fatal(err)
 		}
 
-		got := buf.String()
-		want := `<h1>hello world</h1><p>This is a description</p>Tags: <ul><li>go</li><li>tdd</li></ul>`
-
-		if got != want {
-			t.Errorf("got '%s' want '%s'", got, want)
-		}
+		approvals.VerifyString(t, buf.String())
 	})
-
 }
