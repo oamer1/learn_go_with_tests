809cb114de700ac0d50a627016d41d0130dc9f00
Omar Amer <oamer@windowslive.com>
2022-03-17 11:08:19 +0200

    Green refactor render body

--- blogrenderer/renderer_test.go
+++ blogrenderer/renderer_test.go
@@ -12,14 +12,19 @@ import (
 func TestRender(t *testing.T) {
 	var (
 		aPost = blogrenderer.Post{
-			Title:       "hello world",
-			Body:        "This is a post",
+			Title: "hello world",
+			Body: `# First recipe!
+Welcome to my **amazing blog**. I am going to write about my family recipes, and make sure I write a long, irrelevant and boring story about my family before you get to the actual instructions.`,
 			Description: "This is a description",
 			Tags:        []string{"go", "tdd"},
 		}
 	)
 
-	postRenderer, _ := blogrenderer.NewPostRenderer()
+	postRenderer, err := blogrenderer.NewPostRenderer()
+
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	t.Run("it converts a single post into HTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
@@ -52,7 +57,12 @@ func BenchmarkRender(b *testing.B) {
 			Tags:        []string{"go", "tdd"},
 		}
 	)
-	postRenderer, _ := blogrenderer.NewPostRenderer()
+
+	postRenderer, err := blogrenderer.NewPostRenderer()
+
+	if err != nil {
+		b.Fatal(err)
+	}
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module learn_go_with_tests
 
 go 1.17
 
-require github.com/approvals/go-approval-tests v0.0.0-20220217205408-02c48c44c897 // indirect
+require (
+	github.com/approvals/go-approval-tests v0.0.0-20220217205408-02c48c44c897 // indirect
+	github.com/gomarkdown/markdown v0.0.0-20220310201231-552c6011c0b8 // indirect
+)
