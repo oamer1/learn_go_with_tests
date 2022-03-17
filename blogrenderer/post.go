ccbc63dbb966146710d0a4195926e5b27f872094
Omar Amer <oamer@windowslive.com>
2022-03-17 10:58:03 +0200

    Green sanitized title

--- blogrenderer/post.go
+++ blogrenderer/post.go
@@ -1,7 +1,13 @@
 package blogrenderer
 
+import "strings"
+
 // Post is a representation of a post
 type Post struct {
 	Title, Description, Body string
 	Tags                     []string
+}
+
+func (p Post) SanitisedTitle() string {
+	return strings.ToLower(strings.Replace(p.Title, " ", "-", -1))
 }
