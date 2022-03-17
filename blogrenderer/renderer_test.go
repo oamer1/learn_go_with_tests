74b5387d0b92e6ed24f6306ded495973c302b24f
Omar Amer <oamer@windowslive.com>
2022-03-17 09:55:36 +0200

    Green Refactor solve reparsing issue

--- blogrenderer/renderer_test.go
+++ blogrenderer/renderer_test.go
@@ -2,6 +2,7 @@ package blogrenderer_test
 
 import (
 	"bytes"
+	"io"
 	"learn_go_with_tests/blogrenderer"
 	"testing"
 
@@ -18,13 +19,32 @@ func TestRender(t *testing.T) {
 		}
 	)
 
+	postRenderer, _ := blogrenderer.NewPostRenderer()
+
 	t.Run("it converts a single post into HTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
 
-		if err := blogrenderer.Render(&buf, aPost); err != nil {
+		if err := postRenderer.Render(&buf, aPost); err != nil {
 			t.Fatal(err)
 		}
 
 		approvals.VerifyString(t, buf.String())
 	})
+}
+
+func BenchmarkRender(b *testing.B) {
+	var (
+		aPost = blogrenderer.Post{
+			Title:       "hello world",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+	postRenderer, _ := blogrenderer.NewPostRenderer()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		postRenderer.Render(io.Discard, aPost)
+	}
 }
