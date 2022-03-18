da717b7c0bfae234a474456c234897595f985fb6
Omar Amer <oamer@windowslive.com>
2022-03-18 11:01:47 +0200

    Green server

---
+++ HTTP_server/server.go
@@ -0,0 +1,11 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// PlayerServer currently returns Hello, world given _any_ request.
+func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "20")
+}
