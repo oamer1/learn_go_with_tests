cfef4cb84c33118c46b194f646c7b1348a4d3504
Omar Amer <oamer@windowslive.com>
2022-03-19 22:40:07 +0200

    Green add integeration tests

--- HTTP_server/main.go
+++ HTTP_server/main.go
@@ -5,19 +5,7 @@ import (
 	"net/http"
 )
 
-// InMemoryPlayerStore collects data about players in memory.
-type InMemoryPlayerStore struct{}
-
-// RecordWin will record a player's win.
-func (i *InMemoryPlayerStore) RecordWin(name string) {
-}
-
-// GetPlayerScore retrieves scores for a given player.
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return 123
-}
-
 func main() {
-	server := &PlayerServer{&InMemoryPlayerStore{}}
+	server := &PlayerServer{NewInMemoryPlayerStore()}
 	log.Fatal(http.ListenAndServe(":5000", server))
 }
--- HTTP_server/server_test.go
+++ HTTP_server/server_test.go
@@ -79,7 +79,7 @@ func TestStoreWins(t *testing.T) {
 	t.Run("it records wins on POST", func(t *testing.T) {
 		player := "Pepper"
 
-		request, _ := http.NewRequest(http.MethodPost, fmt.Sprintf("/players/%s", player), nil)
+		request := newPostWinRequest(player)
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
@@ -105,6 +105,11 @@ func assertStatus(t testing.TB, got, want int) {
 
 func newGetScoreRequest(name string) *http.Request {
 	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
+	return req
+}
+
+func newPostWinRequest(name string) *http.Request {
+	req, _ := http.NewRequest(http.MethodPost, fmt.Sprintf("/players/%s", name), nil)
 	return req
 }
 
