d99a42b7eaf922d1484f5ecb4d57a9ec4c4689f8
Omar Amer <oamer@windowslive.com>
2022-03-19 20:48:15 +0200

    Green

--- HTTP_server/main.go
+++ HTTP_server/main.go
@@ -8,6 +8,10 @@ import (
 // InMemoryPlayerStore collects data about players in memory.
 type InMemoryPlayerStore struct{}
 
+// RecordWin will record a player's win.
+func (i *InMemoryPlayerStore) RecordWin(name string) {
+}
+
 // GetPlayerScore retrieves scores for a given player.
 func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
 	return 123
