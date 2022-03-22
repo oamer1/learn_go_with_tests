1e9039552e7c4d206ebfb88d4490268d23cd5454
Omar Amer <oamer@windowslive.com>
2022-03-22 12:18:06 +0200

    Green use empty interface

--- Generics/generics_test.go
+++ Generics/generics_test.go
@@ -4,19 +4,19 @@ import "testing"
 
 func TestAssertFunctions(t *testing.T) {
 	t.Run("asserting on integers", func(t *testing.T) {
-		AssertEqual(t, 1, 1)
+		AssertEqual(t, "a", "a")
 		AssertNotEqual(t, 1, 2)
 	})
 }
 
-func AssertEqual(t *testing.T, got, want int) {
+func AssertEqual(t *testing.T, got, want interface{}) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %d, want %d", got, want)
 	}
 }
 
-func AssertNotEqual(t *testing.T, got, want int) {
+func AssertNotEqual(t *testing.T, got, want interface{}) {
 	t.Helper()
 	if got == want {
 		t.Errorf("didn't want %d", got)
