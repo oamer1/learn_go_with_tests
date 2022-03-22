71cfbbd6f1a5feadb777a68ccced9de45e8e80bf
Omar Amer <oamer@windowslive.com>
2022-03-22 12:50:12 +0200

    Green use comparable interface

--- Generics/generics_test.go
+++ Generics/generics_test.go
@@ -4,21 +4,25 @@ import "testing"
 
 func TestAssertFunctions(t *testing.T) {
 	t.Run("asserting on integers", func(t *testing.T) {
-		AssertEqual(t, "a", "a")
+		AssertEqual(t, 1, 1)
 		AssertNotEqual(t, 1, 2)
+	})
+	t.Run("asserting on strings", func(t *testing.T) {
+		AssertEqual(t, "hello", "hello")
+		AssertNotEqual(t, "hello", "Grace")
 	})
 }
 
-func AssertEqual(t *testing.T, got, want interface{}) {
+func AssertEqual[T comparable](t *testing.T, got, want T) {
 	t.Helper()
 	if got != want {
-		t.Errorf("got %d, want %d", got, want)
+		t.Errorf("got %v, want %v", got, want)
 	}
 }
 
-func AssertNotEqual(t *testing.T, got, want interface{}) {
+func AssertNotEqual[T comparable](t *testing.T, got, want T) {
 	t.Helper()
 	if got == want {
-		t.Errorf("didn't want %d", got)
+		t.Errorf("didn't want %v", got)
 	}
 }
