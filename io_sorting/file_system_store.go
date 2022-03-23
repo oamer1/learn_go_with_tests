2a15d5e90eb7700cc206b3f1377051d160ea935d
Omar Amer <oamer@windowslive.com>
2022-03-23 18:43:31 +0200

    Refactor NewFileSystemPlayerStore

--- io_sorting/file_system_store.go
+++ io_sorting/file_system_store.go
@@ -12,17 +12,11 @@ type FileSystemPlayerStore struct {
 }
 
 func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
-	file.Seek(0, 0)
-	info, err := file.Stat()
+
+	err := initialisePlayerDBFile(file)
 
 	if err != nil {
-		return nil, fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
-	}
-
-	if info.Size() == 0 {
-		file.Write([]byte("[]"))
-		file.Seek(0, 0)
-
+		return nil, fmt.Errorf("problem initialising player db file, %v", err)
 	}
 
 	league, err := NewLeague(file)
@@ -64,4 +58,21 @@ func (f *FileSystemPlayerStore) RecordWin(name string) {
 	// When we RecordWin, we Seek back to the start of the file and then write the new data—but what if the new data was smaller than what was there before?
 
 	f.database.Encode(f.league)
+}
+
+func initialisePlayerDBFile(file *os.File) error {
+	file.Seek(0, 0)
+
+	info, err := file.Stat()
+
+	if err != nil {
+		return fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
+	}
+
+	if info.Size() == 0 {
+		file.Write([]byte("[]"))
+		file.Seek(0, 0)
+	}
+
+	return nil
 }
