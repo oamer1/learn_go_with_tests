2ce4553fd8afca1a544cb8382f51f3c5ef727ef3
Omar Amer <oamer@windowslive.com>
2022-03-29 01:41:34 +0200

    Green before refactor

---
+++ questions_and_answers/OS_Exec/before/os_exec_test.go
@@ -0,0 +1,12 @@
+package before
+
+import "testing"
+
+func TestGetData(t *testing.T) {
+	got := GetData()
+	want := "HAPPY NEW YEAR!"
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
