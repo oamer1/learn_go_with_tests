3e5c025d7599c3f437907ca5e2d3f7a6b46e84f8
Omar Amer <oamer@windowslive.com>
2022-03-29 01:41:45 +0200

    Green after refactor

---
+++ questions_and_answers/OS_Exec/after/os_exec.go
@@ -0,0 +1,31 @@
+package after
+
+import (
+	"bytes"
+	"encoding/xml"
+	"io"
+	"io/ioutil"
+	"os/exec"
+	"strings"
+)
+
+type Payload struct {
+	Message string `xml:"message"`
+}
+
+func GetData(data io.Reader) string {
+	var payload Payload
+	xml.NewDecoder(data).Decode(&payload)
+	return strings.ToUpper(payload.Message)
+}
+
+func getXMLFromCommand() io.Reader {
+	cmd := exec.Command("cat", "msg.xml")
+	out, _ := cmd.StdoutPipe()
+
+	cmd.Start()
+	data, _ := ioutil.ReadAll(out)
+	cmd.Wait()
+
+	return bytes.NewReader(data)
+}
