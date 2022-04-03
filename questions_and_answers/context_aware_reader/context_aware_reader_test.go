d92cd3e22c49a5748b45d37e2ca63c1662e03eac
Omar Amer <oamer@windowslive.com>
2022-04-03 21:16:48 +0200

    Green test normal reader

---
+++ questions_and_answers/context_aware_reader/context_aware_reader_test.go
@@ -0,0 +1,39 @@
+package contextawarereader
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestContextAwareReader(t *testing.T) {
+	t.Run("behaves like a normal reader", func(t *testing.T) {
+		rdr := strings.NewReader("123456")
+		got := make([]byte, 3)
+		_, err := rdr.Read(got)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assertBufferHas(t, got, "123")
+
+		_, err = rdr.Read(got)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assertBufferHas(t, got, "456")
+
+	})
+
+}
+
+func assertBufferHas(t testing.TB, buf []byte, want string) {
+	t.Helper()
+	got := string(buf)
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
