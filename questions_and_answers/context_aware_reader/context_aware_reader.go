c0648e2cd64f73be7bc73c54291687492b8a7536
Omar Amer <oamer@windowslive.com>
2022-04-03 21:39:00 +0200

    Green !

--- questions_and_answers/context_aware_reader/context_aware_reader.go
+++ questions_and_answers/context_aware_reader/context_aware_reader.go
@@ -18,6 +18,12 @@ type readerCtx struct {
 }
 
 func (r *readerCtx) Read(p []byte) (n int, err error) {
+
+	if err := r.ctx.Err(); err != nil {
+		return 0, err
+
+	}
+
 	return r.delegate.Read(p)
 
 }
